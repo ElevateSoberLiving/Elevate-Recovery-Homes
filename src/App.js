bdf98e5d86b2e9588f479eda6dd3ea01d9970b80
Hawleywoo <alhawley21@gmail.com>
2021-04-20 23:44:24 -0600

    Routes to main pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import './App.scss';
+import AboutUs from './pages/AboutUs'
+import Contact from './pages/Contact'
+import OurHouses from './pages/OurHouses'
+import WhyUs from './pages/WhyUs'
+import Faq from './pages/Faq'
+import AboutUs from './pages/AboutUs'
 import FixedHeader from './Components/FixedHeader'
 import Hero from './Components/Hero'
+import { 
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
 
 
 function App() {
@@ -8,7 +19,17 @@ function App() {
     <div className="App">
       <FixedHeader />
       <Hero />
-      
+
+      <Router>
+        <Switch>
+          <Route path="/" exact/>
+          <Route path="/about" component={AboutUs} />
+          <Route path="/our-houses" component={OurHouses}/>
+          <Route path="why-us" component={WhyUs}/>
+          <Route path="faq" component={Faq}/>
+          <Route path="contact" component={Contact}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/OurHouses.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function OurHouses() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default OurHouses
