4be765095cdadeb2252494034099f8bf31985e9f
Hawleywoo <alhawley21@gmail.com>
2021-04-23 23:28:39 -0600

    Footer layout and style

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,32 +8,36 @@ import { FaTwitterSquare } from 'react-icons/fa'
 function Footer() {
     return (
         <div className="footer">
-            <img  className="footer__logo" src={WhiteLogo} alt="Elevate Recovery Logo" />
-            <div className="footer__social">
-                <a target="_blank" rel="noreferrer noopener"><FaFacebookSquare/></a>
-                <a target="_blank" rel="noreferrer noopener"><FaInstagram/></a>
-                <a target="_blank" rel="noreferrer noopener" ><FaTwitterSquare/></a>
+            <img className="footer__logo" src={WhiteLogo} alt="Elevate Recovery Logo" />
+            <div className="footer__mid-container" >
+                <div className="footer__social">
+                    <a target="_blank" rel="noreferrer noopener"><FaFacebookSquare /></a>
+                    <a target="_blank" rel="noreferrer noopener"><FaInstagram /></a>
+                    <a target="_blank" rel="noreferrer noopener" ><FaTwitterSquare /></a>
+                </div>
+                <hr id="footer-break" />
+                <ul className="footer__menu">
+                    <li >
+                        <Link className="links" to="/" >Home</Link>
+                    </li>
+                    <li >
+                        <Link className="links" to="/about">About us</Link>
+                    </li>
+                    <li >
+                        <Link className="links" to="/our-houses">Our Houses</Link>
+                    </li>
+                    <li >
+                        <Link className="links" to="/why-us">About us</Link>
+                    </li>
+                    <li >
+                        <Link className="links" to="/faq">FAQ</Link>
+                    </li>
+                    <li >
+                        <Link className="links" to="/contact">Contact</Link>
+                    </li>
+                </ul>
             </div>
-            <ul className="footer__menu">
-                <li >
-                    <Link className="links" to="/" >Home</Link>
-                </li>
-                <li >
-                    <Link className="links" to="/about">About us</Link>
-                </li>
-                <li >
-                    <Link className="links" to="/our-houses">Our Houses</Link>
-                </li>
-                <li >
-                    <Link className="links" to="/why-us">About us</Link>
-                </li>
-                <li >
-                    <Link className="links" to="/faq">FAQ</Link>
-                </li>
-                <li >
-                    <Link className="links" to="/contact">Contact</Link>
-                </li>
-            </ul>
+            <p id="copyright">© copyright 2021</p>
         </div>
     )
 }
