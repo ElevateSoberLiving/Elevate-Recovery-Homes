646a9878159a66dcf942cbd029cf40344bd7c937
Hawleywoo <alhawley21@gmail.com>
2021-05-25 12:53:44 -0600

    Add images

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '../Assets/Logos/Elevate Logo Files/WEB/Elevate_Logo_Vertical_Color_RGB@2x.png'
+import FoggyMountains from '../Assets/Images/Foggy-Mountains.jpg'
+import Hiking from '../Assets/Images/Guy-Hiking.jpg'
 import Group3 from '../Assets/Images/Stock-Group3.jpg'
 import EmbedVideo from '../Components/EmbedVideo'
 import Hero from '../Components/Hero'
@@ -14,7 +16,7 @@ function Home() {
                 <div >
                     <img className="home__first--logo" src={Logo} alt="Elevate Recovery Logo" />
                     <h2 className="home__first--header" >DENVER’S PREMIER TRANSITIONAL LIVING COMMUNITY FOR ADULT MEN</h2>
-                    <p>Elevate Recovery Homes is a transitional community for adult men who have struggled with substance use and addiction and who have completed primary treatment, are currently in IOP, recently released from detox, or participated in other interventions.<br/><br/> Our typical member is looking for a supportive environment to strengthen their recovery, prevent relapse and integrate newly acquired skills into everyday life.<br/><br/> We provide a safe, supportive, and structured environment focused on helping members develop the independent living skills necessary to transition into a healthier lifestyle successfully. We know that the needs of adult men are unique, so we have developed our community specifically to meet those needs.</p>
+                    <p>Elevate Recovery Homes is a transitional community for adult men who have struggled with substance use and addiction and who have completed primary treatment, are currently in IOP, recently released from detox, or participated in other interventions.<br /><br /> Our typical member is looking for a supportive environment to strengthen their recovery, prevent relapse and integrate newly acquired skills into everyday life.<br /><br /> We provide a safe, supportive, and structured environment focused on helping members develop the independent living skills necessary to transition into a healthier lifestyle successfully. We know that the needs of adult men are unique, so we have developed our community specifically to meet those needs.</p>
                 </div>
             </section>
             <section className="home__second">
@@ -25,14 +27,20 @@ function Home() {
                 <Link className="" to="/about/elevate-recovery-homes">About us philosophy</Link>
             </section>
             <section className="home__third">
-                <h2>WHY CHOOSE US</h2>
-                <p><b>“IF YOU DON’T KNOW WHERE YOU’RE GOING, ANY ROAD WILL TAKE YOU THERE”</b></p>
-                <Link to="/program/why-us">OUR TEAM</Link>
+                <img src={FoggyMountains} id="mountains" alt="Rocky Mountains with peaks higher than clouds"/>
+                <div>
+                    <h2>WHY CHOOSE US</h2>
+                    <p><b>“IF YOU DON’T KNOW WHERE YOU’RE GOING, ANY ROAD WILL TAKE YOU THERE”</b></p>
+                    <Link to="/program/why-us">OUR TEAM</Link>
+                </div>
             </section>
             <section className="home__fourth">
-                <h2>LOCATION</h2>
-                <p>Elevate is located just minutes from Denver’s beautiful Front Range in Englewood—an ideal neighborhood for men looking for a fresh start.</p>
-                <Link to="/location">LOCATION</Link>
+                <div>
+                    <h2>LOCATION</h2>
+                    <p>Elevate is located just minutes from Denver’s beautiful Front Range in Englewood—an ideal neighborhood for men looking for a fresh start.</p>
+                    <Link to="/location">LOCATION</Link>
+                </div>
+                <img src={Hiking} id="hiking" alt="Guy hiking in mountains"/>
             </section>
             <section className="home__fifth">
                 <h2>THE TEAM</h2>
