86838f3561c51ce292c7e1e03cfc7a1e2968b969
Hawleywoo <alhawley21@gmail.com>
2021-06-03 14:30:13 -0600

    House images and responsive tufts page

--- src/Components/TuftsHome.js
+++ src/Components/TuftsHome.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import Stock1 from '../Assets/Images/House-Img/Stock1.jpg'
-import Stock2 from '../Assets/Images/House-Img/Stock2.jpg'
-import Stock3 from '../Assets/Images/House-Img/Stock3.jpg'
-import Stock4 from '../Assets/Images/House-Img/Stock4.jpg'
+import ElevateLiving from '../Assets/Images/House-Img/ElevateLiving.jpg'
+import ElevateKit from '../Assets/Images/House-Img/ElevateKit.jpg'
+import ElevateFront from '../Assets/Images/House-Img/ElevateFront.jpg'
+import ElevateBath from '../Assets/Images/House-Img/ElevateBath.jpg'
+import ElevateSmBed from '../Assets/Images/House-Img/ElevateSmBed.jpg'
+import ElevateChicken from '../Assets/Images/House-Img/ElevateChicken.jpg'
 
 function TuftsHome() {
     return (
@@ -13,10 +15,12 @@ function TuftsHome() {
                 <p>Our Men's Recovery Home is located in Englewood, Colorado. The beautiful, 2,300 square foot house features four bedrooms, two full baths, multiple living room areas, a home gym, a dedicated work desk with computer and printer, a hot tub, and a spacious backyard. It is truly a warm and family-friendly home. The Tufts Home is staffed with a dedicated 24/7 House Manager and Peer Recovery Coach. We have a zero-tolerance drug and alcohol policy and require our members to participate in House meetings and suggest a minimum of five (5) recovery based activities per week and follow all other rules of the House.</p>
             </div>
             <div className="admissions__tufts--img-container">
-                <img id="stock-img1" src={Stock1} alt="stock house" />
-                <img id="stock-img2" src={Stock2} alt="stock house" />
-                <img id="stock-img3" src={Stock3} alt="stock house" />
-                <img id="stock-img4" src={Stock4} alt="stock house" />
+                <img id="living-img1" src={ElevateLiving} alt="stock house" />
+                <img id="kitchen-img2" src={ElevateKit} alt="stock house" />
+                <img id="front-house-img3" src={ElevateFront} alt="stock house" />
+                <img id="bathroom-img4" src={ElevateBath} alt="stock house" />
+                <img id="small-bed-img5" src={ElevateSmBed} alt="stock house" />
+                <img id="chicken-img6" src={ElevateChicken} alt="stock house" />
             </div>
         </section>
     )
