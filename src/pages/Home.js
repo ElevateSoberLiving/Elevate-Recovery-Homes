08627965b8cafcdd21c9e851956ecbdaef886dad
Hawleywoo <alhawley21@gmail.com>
2021-06-09 00:17:26 -0600

    Video

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ import HikingDesert from '../Assets/Images/Hiking-Desert-min.jpg'
 import EmbedVideo from '../Components/EmbedVideo'
 import Hero from '../Components/Hero'
 import { FaArrowRight } from 'react-icons/fa'
-// import Tour from '../Assets/Video/Tour.mp4'
+import Tour from '../Assets/Video/Tour.mp4'
 
 function Home() {
     return (
@@ -69,7 +69,7 @@ function Home() {
                     <h2>THE HOUSE</h2>
                     <Link className="home__links" id="house-link" to="/admissions/tufts-home">MORE ABOUT TUFTS HOME</Link>
                 </div>
-                <EmbedVideo videoId={''} />
+                <EmbedVideo videoId={Tour} />
             </section>
             <section className="home__eighth">
                 <h2>GET IN TOUCH</h2>
