6563211b5aa209d6bcba836edfa2b8355331c5c9
Hawleywoo <alhawley21@gmail.com>
2021-06-10 17:43:00 -0600

    netlify form

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -1,18 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const initialState = {
+    name: '',
+    phoneNumber: '',
+    email: '',
+    comments: ''
+}
+
+const encode = (data) => {
+    return Object.keys(data).map(key => {
+        return encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
+    }).join("&")
+}
 
 function ContactForm() {
+    const [formData, setFormData] = useState(initialState)
+    const [formSuccess, setFormSuccess] = useState(false)
+
+
+    const handleSubmit = ( event ) => {
+        event.preventDefault()
+        fetch("/", {
+            method: 'POST',
+            headers: { "Content-Type": 'application/x-www-form-urlencoded'},
+            body: encode({ 
+                "form-name": "contact",
+                ...formData
+            })
+            .then(() => setFormSuccess(!formSuccess))
+            .catch(error => alert(error))
+
+        })
+
+    }
+
+    const handleChange = ( event ) => {
+        setFormData({
+            [event.target.name]: event.target.value
+        })
+    }
+
     return (
-        <form className="contact__form" netlify>
-            <label>Name:</label>
-            <input name="name"/>
-            <label>Phone Number:</label>
-            <input name="phone-number"/>
-            <label>Email Address:</label>
-            <input type="email" name="email" />
-            <label>Comments</label>
-            <textarea name="comments"/>
-            <input type="submit" value="Contact Us"/>
-        </form>
+        <div>
+            {formSuccess ?
+                <h3>Thank you for interest well be in contact soon</h3>
+                :
+                <form className="contact__form" name="contact" onSubmit={handleSubmit}>
+                    <label>Name:</label>
+                    <input name="name" value={formData.name} onChange={handleChange}/>
+                    <label>Phone Number:</label>
+                    <input name="phoneNumber" value={formData.phoneNumber} onChange={handleChange}/>
+                    <label>Email Address:</label>
+                    <input type="email" name="email" value={formData.email} onChange={handleChange}/>
+                    <label>Comments</label>
+                    <textarea name="comments" value={formData.comments} onChange={handleChange}/>
+                    <input type="hidden" name="contact" value="name_of_my_form" />
+                    <input type="hidden" />
+                    <input type="submit" value="Contact Us"/>
+                </form>
+            }
+        </div>
     )
 }
 
