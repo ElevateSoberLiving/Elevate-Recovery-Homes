38927168eddc87935023ef395355a1e25a99235a
Hawleywoo <alhawley21@gmail.com>
2021-06-20 21:27:58 -0600

    Styling changes inmenu and contact form

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -1,75 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-// const initialState = {
-//     name: '',
-//     phoneNumber: '',
-//     email: '',
-//     comments: ''
-// }
+const initialState = {
+    name: '',
+    phoneNumber: '',
+    email: '',
+    comments: ''
+}
 
-// const encode = (data) => {
-//     return Object.keys(data).map(key => {
-//         return encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
-//     }).join("&")
-// }
+const encode = (data) => {
+    return Object.keys(data).map(key => {
+        return encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
+    }).join("&")
+}
 
 function ContactForm() {
-    // const [formData, setFormData] = useState(initialState)
-    // const [formSuccess, setFormSuccess] = useState(false)
+    const [formData, setFormData] = useState(initialState)
+    const [formSuccess, setFormSuccess] = useState(false)
 
 
-    // const handleSubmit = ( event ) => {
-    //     event.preventDefault()
-    //     fetch("/", {
-    //         method: 'POST',
-    //         headers: { "Content-Type": 'application/x-www-form-urlencoded'},
-    //         body: encode({ 
-    //             "form-name": "contact",
-    //             ...formData
-    //         })
-    //         .then(() => {
-    //             setFormSuccess(!formSuccess)
-    //             setFormData(initialState)
-    //         })
-    //         .catch(error => alert(error))
+    const handleSubmit = ( event ) => {
+        console.log(formData)
+        event.preventDefault()
+        fetch("/", {
+            method: 'POST',
+            headers: { "Content-Type": 'application/x-www-form-urlencoded'},
+            body: encode({ 
+                "form-name": "contact",
+                ...formData
+                })
+            })
+            .then((message) => {
+                console.log(message)
+                setFormSuccess(!formSuccess)
+                setFormData(initialState)
+            })
+            .catch(error => {
+                console.log(error)
+                alert(error)
+            })
+    }
 
-    //     })
+    const handleChange = ( event ) => {
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value
+        })
+    }
 
-    // }
-
-    // const handleChange = ( event ) => {
-    //     setFormData({
-    //         ...formData,
-    //         [event.target.name]: event.target.value
-    //     })
-    // }
-
-    // <form className="contact__form" name="contact" onSubmit={handleSubmit} method="post">
-    //                 <label>Name:</label>
-    //                 <input name="name" value={formData.name} onChange={handleChange}/>
-    //                 <label>Phone Number:</label>
-    //                 <input name="phoneNumber" value={formData.phoneNumber} onChange={handleChange}/>
-    //                 <label>Email Address:</label>
-    //                 <input type="email" name="email" value={formData.email} onChange={handleChange}/>
-    //                 <label>Comments</label>
-    //                 <textarea name="comments" value={formData.comments} onChange={handleChange}/>
-    //                 <input type="hidden" name="contact" value="contact" />
-    //                 <input type="submit" value="Contact Us"/>
-    //             </form> 
     return (
         <div>
             { false ?
                 <h3>Thank you for interest well be in contact soon</h3>
                 :
-                <form className="contact__form" name="contact"  method="post">
+                <form className="contact__form" name="contact"  method="post" onSubmit={handleSubmit} >
                     <label>Name:</label>
-                    <input name="name"/>
+                    <input name="name"value={formData.name} onChange={handleChange} />
                     <label>Phone Number:</label>
-                    <input name="phoneNumber"/>
+                    <input name="phoneNumber" value={formData.phoneNumber} onChange={handleChange}/>
                     <label>Email Address:</label>
-                    <input type="email" name="email" />
+                    <input type="email" name="email" value={formData.email} onChange={handleChange}/>
                     <label>Comments</label>
-                    <textarea name="comments"/>
+                    <textarea id="" name="comments" value={formData.comments} onChange={handleChange}/>
                     <input type="hidden" name="contact" value="contact" />
                     <input type="submit" value="Contact Us"/>
                 </form>
