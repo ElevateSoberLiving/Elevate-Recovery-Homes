08b5b042a494e4ff905979f8c3f926d758114d50
Hawleywoo <alhawley21@gmail.com>
2022-01-30 20:46:58 -0700

    Change hero

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -7,7 +7,7 @@ function Hero() {
         <div className="hero">
             <img src={FoggyMountains} alt="Mountain peaks above the clouds" />
             <div className="hero__title">
-                <h1 className="hero__title--header"><span>L</span>IVE <span>W</span>ITH <span>I</span>NTENTION</h1>
+                <h1 className="hero__title--header"><span>L</span>IVE <span>I</span>NTENTIONALLY</h1>
                 <hr/>
                 <Link className="hero__title--button" to="/program/why-us" >WHY US</Link>
             </div>
