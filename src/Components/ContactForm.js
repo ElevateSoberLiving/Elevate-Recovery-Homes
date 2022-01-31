867e40c3b56a76e45717ed7ab8adc13000b8db4b
Hawleywoo <alhawley21@gmail.com>
2022-01-30 20:50:58 -0700

    For netlify

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -1,56 +1,56 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-const initialState = {
-    name: '',
-    phoneNumber: '',
-    email: '',
-    comments: ''
-}
+// const initialState = {
+//     name: '',
+//     phoneNumber: '',
+//     email: '',
+//     comments: ''
+// }
 
-const encode = (data) => {
-    return Object.keys(data).map(key => {
-        return encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
-    }).join("&")
-}
+// const encode = (data) => {
+//     return Object.keys(data).map(key => {
+//         return encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
+//     }).join("&")
+// }
 
 function ContactForm() {
-    const [formData, setFormData] = useState(initialState)
-    const [formSuccess, setFormSuccess] = useState(false)
+    // const [formData, setFormData] = useState(initialState)
+    // const [formSuccess, setFormSuccess] = useState(false)
 
 
-    const handleSubmit = ( event ) => {
-        console.log(formData)
-        event.preventDefault()
-        fetch("/", {
-            method: 'POST',
-            headers: { "Content-Type": 'application/x-www-form-urlencoded'},
-            body: encode({ 
-                "form-name": "contact",
-                ...formData
-                })
-            })
-            .then((message) => {
-                setFormSuccess(!formSuccess)
-                setFormData(initialState)
-            })
-            .catch(error => {
-                alert(error)
-            })
-    }
+    // const handleSubmit = ( event ) => {
+    //     console.log(formData)
+    //     event.preventDefault()
+    //     fetch("/", {
+    //         method: 'POST',
+    //         headers: { "Content-Type": 'application/x-www-form-urlencoded'},
+    //         body: encode({ 
+    //             "form-name": "contact",
+    //             ...formData
+    //             })
+    //         })
+    //         .then((message) => {
+    //             setFormSuccess(!formSuccess)
+    //             setFormData(initialState)
+    //         })
+    //         .catch(error => {
+    //             alert(error)
+    //         })
+    // }
 
-    const handleChange = ( event ) => {
-        setFormData({
-            ...formData,
-            [event.target.name]: event.target.value
-        })
-    }
+    // const handleChange = ( event ) => {
+    //     setFormData({
+    //         ...formData,
+    //         [event.target.name]: event.target.value
+    //     })
+    // }
 
     return (
         <div className="contact__form">
-            { formSuccess ?
+            { true ?
                 <h3 id="success-message">THANK YOU FOR YOUR INTEREST WE WILL BE IN CONTACT SOON</h3>
                 :
-                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSer_CXzGXLVpYM-7s1NBXEHm6J5mNb1eUWErN3wzpRSd3ZOAQ/viewform?embedded=true" width="700" height="520" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSer_CXzGXLVpYM-7s1NBXEHm6J5mNb1eUWErN3wzpRSd3ZOAQ/viewform?embedded=true" width="700" height="520" frameborder="0" marginheight="0" marginwidth="0" title="Elevate Recovery Home Intake/Contact Form">Loading…</iframe>
             }
         </div>
     )
